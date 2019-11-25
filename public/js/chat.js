4fcfab47d6aeb87f16be18fc9a25514bcfbf3053
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2019-11-25 18:28:22 +0800

    acknowledements

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,5 +7,24 @@ socket.on('message', (message) => {
 document.querySelector('#typed-message').addEventListener('submit', (e) => {
     e.preventDefault()
     const msg = e.target.elements.message.value
-    socket.emit('submitted', msg)
+    socket.emit('submitted', msg, (error) => {
+        if (error) {
+            return console.log(error)
+        }
+        console.log('message delivered')
+    })
+})
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return 'Geolocation is not available'
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, () => {
+            console.log('location shared')
+        })
+    })
 })
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 
 const app = express()
@@ -16,11 +17,22 @@ io.on('connection', (socket) => {
     console.log('New socket connection')
 
     socket.emit('message', 'welcome')
-
-    socket.on('submitted', (message) => {
+    socket.broadcast.emit('message', 'A new user has joined')
+    socket.on('submitted', (message, callback) => {
+        const filter = new Filter()
+        if (filter.isProfane(message)) {
+            callback('Profanity is not allowed')
+        }
         io.emit('message', message)
+        callback()
     })
-
+    socket.on('sendLocation', (coords, callback) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        callback()
+    })
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left')
+    })
 })
 server.listen(port, () => {
     console.log(`app is running on port ${port}`)
